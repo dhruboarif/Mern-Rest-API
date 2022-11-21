f51e4c33d10fc62461ca0adf90c286f4be821470
[dhruboarif] <[ariful48@gmail.com]>
2022-11-21 17:49:33 +0600

    final assign mongoose crud

---
+++ Controllers/MongooseCrud.js
@@ -0,0 +1,103 @@
+const UserModel = require("../Model/UserModel");
+const bcrypt = require('bcryptjs');
+
+exports.getUser = (req, res) => {
+    let Query = {};
+    let Projection = "Name email";
+    UserModel.find(Query, Projection, (err, data) => {
+        if (err) {
+            res.status(400).json({ status: "Fail", data: err });
+        } else {
+            res.status(201).json({ status: "success", data: data })
+        }
+    })
+}
+
+exports.createUser = async (req, res) => {
+
+    try {
+        console.log(req.body);
+        const { first_name, last_name, email, password, role } = req.body;
+
+        // Validate user input
+        if (!(email && password && first_name && last_name)) {
+            res.status(400).send("All input is required");
+        }
+
+        const oldUser = await UserModel.findOne({ email });
+
+        if (oldUser) {
+            return res.status(409).send("User Already Exist. Please Login");
+        }
+
+        encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await UserModel.create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(),
+            role,// sanitize: convert email to lowercase
+            password: encryptedPassword,
+        });
+        res.status(200).send("Done");
+    } catch { };
+}
+
+
+exports.login = async (req, res) => {
+
+    // Our login logic starts here
+    try {
+        // Get user input
+        const { email, password } = req.body;
+
+        // Validate user input
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+        // Validate if user exist in our database
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            // user
+            res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+    } catch (err) {
+        console.log(err);
+    }
+    // Our register logic ends here
+};
+
+exports.changePassword = async (req, res) => {
+
+    // Our login logic starts here
+    try {
+        // Get user input
+        const { email, password, newPassword } = req.body;
+
+        // Validate user input
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+        // Validate if user exist in our database
+        const user = await UserModel.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            // user
+            UserModel.findByIdAndUpdate(user.id, { password: newPassword }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(data);
+                }
+            });
+        }
+        res.status(400).send("Password changed successfully");
+    } catch (err) {
+        console.log(err);
+    }
+    // Our register logic ends here
+}; 
--- routes/userApi.js
+++ routes/userApi.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express('router');
 const bcrypt = require('bcryptjs');
+const UserController = require('../Controllers/MongooseCrud');
 
 // router.get('/', (req, res) => {
 //     res.send('Hello World!')
@@ -14,94 +15,13 @@ const bcrypt = require('bcryptjs');
 const User = require("../Model/UserModel");
 
 // Register
-router.post("/register", async (req, res) => {
-    try {
-
-        console.log(req.body);
-        const { first_name, last_name, email, password, role } = req.body;
-
-        // Validate user input
-        if (!(email && password && first_name && last_name)) {
-            res.status(400).send("All input is required");
-        }
-
-        const oldUser = await User.findOne({ email });
-
-        if (oldUser) {
-            return res.status(409).send("User Already Exist. Please Login");
-        }
-
-        encryptedPassword = await bcrypt.hash(password, 10);
-
-        const user = await User.create({
-            first_name,
-            last_name,
-            email: email.toLowerCase(),
-            role,// sanitize: convert email to lowercase
-            password: encryptedPassword,
-        });
-        res.status(200).send("Done");
-    } catch { }
-});
+router.post("/register", UserController.createUser);
 
 // Login
-router.post("/login", async (req, res) => {
+router.post("/login", UserController.login);
 
-    // Our login logic starts here
-    try {
-        // Get user input
-        const { email, password } = req.body;
+router.post("/changepassword", UserController.changePassword);
 
-        // Validate user input
-        if (!(email && password)) {
-            res.status(400).send("All input is required");
-        }
-        // Validate if user exist in our database
-        const user = await User.findOne({ email });
-
-        if (user && (await bcrypt.compare(password, user.password))) {
-            // Create token
-            // user
-            res.status(200).json(user);
-        }
-        res.status(400).send("Invalid Credentials");
-    } catch (err) {
-        console.log(err);
-    }
-    // Our register logic ends here
-});
-
-
-router.post("/changepassword", async (req, res) => {
-
-    // Our login logic starts here
-    try {
-        // Get user input
-        const { email, password, newPassword } = req.body;
-
-        // Validate user input
-        if (!(email && password)) {
-            res.status(400).send("All input is required");
-        }
-        // Validate if user exist in our database
-        const user = await User.findOne({ email });
-
-        if (user && (await bcrypt.compare(password, user.password))) {
-            // Create token
-            // user
-            User.findByIdAndUpdate(user.id, { password: newPassword }, (err, data) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(data);
-                }
-            });
-        }
-        res.status(400).send("Password changed successfully");
-    } catch (err) {
-        console.log(err);
-    }
-    // Our register logic ends here
-});
+router.get("/getuser", UserController.getUser);
 
 module.exports = router; 
